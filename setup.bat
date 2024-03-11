9b93ecbd33a749624fde134c13a134fd85969ec4
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-03-11 11:26:23 -0700

    Update setup script to match quickstart guide
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- setup.bat
+++ setup.bat
@@ -4,14 +4,19 @@ goto:$Main
 :$Main
 setlocal EnableDelayedExpansion
     set "PYTHONPATH=%~dp0"
+    proto >nul 2>&1
+    if errorlevel 1 set "PATH=%USERPROFILE%\.proto\bin;%PATH%%"
     set "proto=%USERPROFILE%\.proto\bin\proto.exe"
     cd /D "%~dp0"
     set "python="%proto%" run python -- "
     if not exist "%proto%" set "python=py -3"
-    "%proto%" upgrade
-    "%proto%" use
+    call "%proto%" upgrade
+    call "%proto%" use
     call !python! -m pip install --upgrade pip
-    call !python! -m pip install -r requirements.txt
-    call !python! -m poetry run pip install -r requirements.txt
-    call !python! -m poetry run python scripts/setup
+    call "%proto%" run python -- -m venv "%~dp0.venv"
+    call "%~dp0.venv\Scripts\activate.bat"
+    call python -m pip install --upgrade pip poetry
+    call poetry install --with "ui,local"
+    call python "%~dp0scripts\setup"
+    call poetry run python -m private_gpt
 endlocal & exit /b %errorlevel%
