92c1fbb5359713036e81c15c0cca76e3390552d9
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-05-28 20:13:44 -0700

    More fixes

--- src/personal_gpt/main.py
+++ src/personal_gpt/main.py
@@ -1,11 +1,12 @@
 """FastAPI app creation, logger configuration and main API routes."""
 
-import llama_index
+import llama_index  # type: ignore, pylint: disable=unused-import
+from llama_index.core.callbacks.global_handlers import set_global_handler
 
 from personal_gpt.di import global_injector
 from personal_gpt.launcher import create_app
 
 # Add LlamaIndex simple observability
-llama_index.set_global_handler("simple")
+set_global_handler("simple")
 
 app = create_app(global_injector)
